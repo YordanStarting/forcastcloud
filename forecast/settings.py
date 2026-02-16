3ba0dca2e853525090ad54d39dd3e9d26ff453a4
YordanStarting <moncayo0514@gmail.com>
2026-02-16 16:23:54 -0500

    a

--- forecast/settings.py
+++ forecast/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-nxn2vfnver=%tn_fqk&(&@9ow8!vvh-&g0%3c9l6(e@$m$9$)m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['YordanStarting.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'YordanStarting.pythonanywhere.com']
 
 
 # Application definition
